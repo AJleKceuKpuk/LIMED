c8fbf52e1706fcdbd3ee146446daf341a7edab08
Kpuk <sonich.alexey@gmail.com>
2025-04-10 22:02:01 +0300

    firs-commit

---
+++ limed-backend/src/main/java/com/limed_backend/LimedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.limed_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimedBackendApplication.class, args);
+	}
+
+}
---
+++ limed-backend/src/test/java/com/limed_backend/LimedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limed_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimedBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
